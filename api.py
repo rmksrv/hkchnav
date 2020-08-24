a30612d7bafc2142f58637240571978b3af70833
Roman Kosarev <ordoshouse@tuta.io>
2020-08-24 18:16:13 +0300

    added navigator

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 import os
+import re
 import colorama as cr
 from requests import get
 
@@ -14,6 +15,13 @@ class Gist:
     def cout(self):
         print('Gist from \'{}\''.format(self.request_url))
 
+    def parse_html(self, src):
+        res = src
+        res = re.sub('\<\/a\>', cr.Fore.RESET, res)
+        #res = re.sub(r'\<a .*\S\>', cr.Fore.CYAN + '<aga>' , res)
+        res = re.sub('<br>', '\n' , res)
+        return res
+
 
 class Attachment(Gist):
     def __init__(self, path):
@@ -28,8 +36,9 @@ class Attachment(Gist):
             return None
 
     def cout(self):
-        print('Att. {c}{link}'.format(
-            c=cr.Fore.CYAN,
+        print('Att. {co}{link}{cc}'.format(
+            co=cr.Fore.CYAN,
+            cc=cr.Fore.RESET,
             link=self.get(),
         ))
 
@@ -46,22 +55,27 @@ class Post(Gist):
         self.files = files
 
     def cout(self):
-        print('{s}{f}--------------------------------------------------------------------------------'.format(
-            s=cr.Style.BRIGHT,
-            f=cr.Fore.BLACK,
+        print('{so}{fo}--------------------------------------------------------------------------------{sc}{fc}'.format(
+            so=cr.Style.BRIGHT,
+            fo=cr.Fore.BLACK,
+            sc=cr.Style.RESET_ALL,
+            fc=cr.Fore.RESET,
         ))
-        print('{b}{sub} - {name} {date} {num}'.format(
+        print('{bo}{sub} - {name} {date} {num}{bc}'.format(
             sub=self.subject,
             name=self.name,
             date=self.date,
             num=self.num,
-            b=cr.Back.GREEN,
+            bo=cr.Back.GREEN,
+            bc=cr.Back.RESET,
         ))
-        print(self.comment)
+        print(self.parse_html(self.comment))
         if self.files:
-            print('{s}{f}________________________________________________________________________________'.format(
-                s=cr.Style.BRIGHT,
-                f=cr.Fore.BLACK,
+            print('{so}{fo}________________________________________________________________________________{sc}{fc}'.format(
+                so=cr.Style.BRIGHT,
+                fo=cr.Fore.BLACK,
+                sc=cr.Style.RESET_ALL,
+                fc=cr.Fore.RESET,
             ))
             for f in self.files:
                 f.cout()
@@ -78,16 +92,20 @@ class Thread(Gist):
 
     def cout(self):
         jsn = self.get()
-        print('{f}{b}/{code}/ - {board}'.format(
+        print('{fo}{bo}/{code}/ - {board}{fc}{bc}'.format(
             code=jsn['Board'],
             board=jsn['BoardName'],
-            f=cr.Fore.BLACK,
-            b=cr.Back.YELLOW,
+            fo=cr.Fore.BLACK,
+            bo=cr.Back.YELLOW,
+            fc=cr.Fore.RESET,
+            bc=cr.Back.RESET,
         ))
-        print('{f}{s}{descr}'.format(
+        print('{fo}{so}{descr}{fc}{sc}'.format(
             descr=jsn['BoardInfoOuter'],
-            f=cr.Fore.YELLOW,
-            s=cr.Style.BRIGHT,
+            fo=cr.Fore.YELLOW,
+            so=cr.Style.BRIGHT,
+            fc=cr.Fore.RESET,
+            sc=cr.Style.RESET_ALL,
         ))
         for jsn_post in jsn['threads'][0]['posts']:
             post = Post(
@@ -114,30 +132,37 @@ class Board(Gist):
 
     def cout(self, page='0'):
         jsn = self.get(page)
-        print('{f}{b}/{code}/ - {board}'.format(
+        print('{fo}{bo}/{code}/ - {board}{fc}{bc}'.format(
             code=jsn['Board'],
             board=jsn['BoardName'],
-            f=cr.Fore.BLACK,
-            b=cr.Back.YELLOW,
+            fo=cr.Fore.BLACK,
+            bo=cr.Back.YELLOW,
+            fc=cr.Fore.RESET,
+            bc=cr.Back.RESET,
         ))
-        print('{f}{s}{descr}'.format(
+        print('{fo}{so}{descr}{fc}{sc}'.format(
             descr=jsn['BoardInfoOuter'],
-            f=cr.Fore.YELLOW,
-            s=cr.Style.BRIGHT,
+            fo=cr.Fore.YELLOW,
+            so=cr.Style.BRIGHT,
+            fc=cr.Fore.RESET,
+            sc=cr.Style.RESET_ALL,
         ))
         for jsn_thread in jsn['threads']:
-            print('{b_num}{f_num}{num}{b_sub}{f_sub}\t- {sub} - {f_descr}{descr}... {f_date}{s_date}-- {date}'.format(
+            print('{bo_num}{fo_num}{num}{bo_sub}{fo_sub}\t- {sub} - {fo_descr}{descr}... {fo_date}{so_date}-- {date}{sc}{fc}{bc}'.format(
                 num=jsn_thread['posts'][0]['num'],
                 sub=jsn_thread['posts'][0]['subject'],
                 date=jsn_thread['posts'][0]['date'],
                 descr=jsn_thread['posts'][0]['comment'][:60],
-                f_num=cr.Fore.BLACK,
-                b_num=cr.Back.CYAN,
-                f_sub=cr.Fore.CYAN,
-                b_sub=cr.Back.RESET,
-                f_date=cr.Fore.BLACK,
-                s_date=cr.Style.BRIGHT,
-                f_descr=cr.Fore.RESET,
+                fo_num=cr.Fore.BLACK,
+                bo_num=cr.Back.CYAN,
+                fo_sub=cr.Fore.CYAN,
+                bo_sub=cr.Back.RESET,
+                fo_date=cr.Fore.BLACK,
+                so_date=cr.Style.BRIGHT,
+                fo_descr=cr.Fore.RESET,
+                fc=cr.Fore.RESET,
+                sc=cr.Style.RESET_ALL,
+                bc=cr.Back.RESET,
             ))
 
 
@@ -148,49 +173,74 @@ class MainPage(Gist):
 
     def cout(self):
         jsn = self.get()
-        print('{b}{f}Двач'.format(
-            b=cr.Back.YELLOW,
-            f=cr.Fore.BLACK,
+        print('{bo}{fo}Двач{bc}{fc}'.format(
+            bo=cr.Back.YELLOW,
+            fo=cr.Fore.BLACK,
+            fc=cr.Fore.RESET,
+            bc=cr.Style.RESET_ALL,
         ))
-        print('{s}{f}--------------------------------------------------------------------------------'.format(
-            s=cr.Style.BRIGHT,
-            f=cr.Fore.BLACK,
+        print('{so}{fo}--------------------------------------------------------------------------------{sc}{fc}'.format(
+            so=cr.Style.BRIGHT,
+            fo=cr.Fore.BLACK,
+            fc=cr.Fore.RESET,
+            sc=cr.Style.RESET_ALL,
         ))
         for category in jsn:
-            print('{s}{cat}'.format(
+            print('{so}{cat}{sc}'.format(
                 cat=category,
-                s=cr.Style.BRIGHT,
+                so=cr.Style.BRIGHT,
+                sc=cr.Style.RESET_ALL,
             ))
             for board in jsn[category]:
                 #print(board)
-                print('\t{f}{id} - {name}'.format(
+                print('\t{fo}{id} - {name}{fc}'.format(
                     id=board['id'],
                     name=board['name'],
-                    f=cr.Fore.CYAN,
+                    fo=cr.Fore.CYAN,
+                    fc=cr.Fore.RESET,
                 ))
 
 
-# TODO
-#class Navigator:
-#    def __init__(self):
-#        self.current_pos =
+class Navigator:
+    def __init__(self):
+        self.current_pos = '/'
+
+    def goto(self, dst='/'):
+        board, page, thread = self.parse_dest(dst)
+        print(board, page, thread)
+        if dst == 'q':
+            return 0
+        else:
+            if board:
+                if thread:
+                    th = Thread(board, thread)
+                    th.cout()
+                else:
+                    bd = Board(board)
+                    bd.cout(page)
+            else:
+                mp = MainPage()
+                mp.cout()
+            return 1
+
+    def parse_dest(self, dst):
+        board = page = thread = None
+        splitted_dst = list(filter(lambda a: a != '', dst.split('/')))
+        if len(splitted_dst) != 0:
+            foo = splitted_dst[0].split('-')
+            board = foo[0]
+            page = foo[1] if len(foo) > 1 else '0'
+            if len(splitted_dst) == 2:
+                thread = splitted_dst[1]
+        return board, page, thread
+
+    def run_nav(self):
+        dst = '/'
+        while self.goto(dst) != 0:
+            dst = input('> ')
 
 
 if __name__ == '__main__':
-    cr.init(autoreset=True)
-    #sample_board = Board('electrach')
-    #sample_board.cout()
-    s = input('Select (1 - Main Page; 2 - Sample board; 3 - Sample Thread): ')
-    print(s)
-    if s == '1':
-        sample_main_page = MainPage()
-        sample_main_page.cout()
-    elif s == '2':
-        sample_board = Board('electrach')
-        sample_board.cout()
-    elif s == '3':
-        sample_thread = Thread('electrach', '18140')
-        sample_thread.cout()
-    else:
-        print('fuck ', s)
-
+    cr.init()
+    my_nav = Navigator()
+    my_nav.run_nav()
